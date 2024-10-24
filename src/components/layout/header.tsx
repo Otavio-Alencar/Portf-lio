164c13e87485a3d689db878d5d3b64c216f1323b
otavio <oalencardev@gmail.com>
2024-10-24 14:40:52 -0300

    fix: build error

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,12 +1,13 @@
 import { HeaderData } from '@/data/header'
 import { Sheet, SheetContent, SheetTrigger } from '../ui/sheet'
 import { Menu } from 'lucide-react'
+import Image from 'next/image'
 export const Header = () => {
   return (
     <div className="fixed z-50 h-28 w-full bg-background shadow-lg">
       <div className="container mx-auto flex h-full items-center justify-between px-4 xl:px-0">
         <div className="flex h-full items-center gap-1">
-          <img src="/logo-branca.png" alt="logo" />
+          <Image src="/logo-branca.png" alt="logo" />
           <p className="font-semibold text-white">Oalencar Dev</p>
         </div>
         <menu className="hidden gap-5 text-white lg:flex">
